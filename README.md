5498be1c54a31709518906c8ebb713ffa6ae4116
Hendi Santika <hendisantika@gmail.com>
2019-01-21 03:34:00 +0700

    #7 - Add Movies Integration Test

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # spring-boot-jpa-optimistic-locking
+
+An example project to demonstrate optimistic locking with java (jpa). The spring-boot setup is just used to wrap all together with a few lines of code.
+
+Run the Unit & Integration Test by this command : `./mvnw clean test`
--- src/test/java/com/hendisantika/springbootjpaoptimisticlocking/SpringBootJpaOptimisticLockingApplicationTests.java
+++ src/test/java/com/hendisantika/springbootjpaoptimisticlocking/SpringBootJpaOptimisticLockingApplicationTests.java
@@ -1,17 +1,68 @@
 package com.hendisantika.springbootjpaoptimisticlocking;
 
+import com.hendisantika.springbootjpaoptimisticlocking.domain.Movie;
+import com.hendisantika.springbootjpaoptimisticlocking.dto.MovieDTO;
+import com.hendisantika.springbootjpaoptimisticlocking.dto.Movies;
+import com.hendisantika.springbootjpaoptimisticlocking.repository.MovieRepository;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootJpaOptimisticLockingApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        movieRepository.save(new Movie("The great movie", 5));
+        movieRepository.save(new Movie("The not so great movie", 4));
+        movieRepository.save(new Movie("Yet another movie", 3));
+        movieRepository.save(new Movie("The big crap", 1));
     }
+
+    /**
+     * Tests the situation, that there are two concurrent users working with a web application
+     * and both operating on the same data at the same time.
+     */
+    @Test(expected = ObjectOptimisticLockingFailureException.class)
+    public void testConcurrencyWriting() {
+        assertEquals("Number of movies in Database incorrect.", 4, movieRepository.count());
+
+        Movie theGreatMovieForUserOne = movieRepository.findByTitle("The great movie");
+        Movie theGreatMovieForUserTwo = movieRepository.findByTitle("The great movie");
+
+        // At first, serialize the movies to transport them to a view.
+        MovieDTO theGreatMovieDTOForUserOne = Movies.of(theGreatMovieForUserOne);
+        MovieDTO theGreatMovieDTOForUserTwo = Movies.of(theGreatMovieForUserTwo);
+
+        // The view modifies the transport objects.
+        theGreatMovieDTOForUserOne.setRating(1);
+        theGreatMovieDTOForUserTwo.setRating(0);
+
+        // The view sends the transport objects to the backend.
+        Movie theUpdatedGreatMovieForUserOne = Movies.of(theGreatMovieDTOForUserOne);
+        Movie theUpdatedGreatMovieForUserTwo = Movies.of(theGreatMovieDTOForUserTwo);
+
+        // The versions of the updateded movies are both 0.
+        assertEquals(0, theUpdatedGreatMovieForUserOne.getVersion().intValue());
+        assertEquals(0, theUpdatedGreatMovieForUserTwo.getVersion().intValue());
+
+        // The backend tries to save both.
+        movieRepository.save(theUpdatedGreatMovieForUserOne);
+
+        // OUTCH! Exception!
+        movieRepository.save(theUpdatedGreatMovieForUserTwo);
+    }
+
 
 }
 

55bdf0c1a47b117312a542afc77f5b1c16342329
Hendi Santika <hendisantika@gmail.com>
2019-01-21 03:27:43 +0700

    #1 - first commit

---
+++ src/main/java/com/hendisantika/springbootjpaoptimisticlocking/SpringBootJpaOptimisticLockingApplication.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springbootjpaoptimisticlocking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaOptimisticLockingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaOptimisticLockingApplication.class, args);
+    }
+
+}
+
